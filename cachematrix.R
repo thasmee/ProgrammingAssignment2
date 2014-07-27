112fdc57eca1d300f82cb73d0d81dd4b197c3010
thasmee <thasmee@dsv.su.se>
2014-07-27 10:58:31 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code computes the invese of a invertible square matrix
+## Lexical scoping enabled 
 
-## Write a short comment describing this function
+## This function creates a matrix object that can cache its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y){
+    x <<- y
+    s <<- NULL
+  }
+  get <- function()x
+  setsolve <- function(solve)s <<- solve
+  getsolve <- function()s
+  list(set = set, get = get, setsolve = setsolve, getsolve =getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix returned 
+##by the function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if (!is.null(s)) {
+    message("getting cashed data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
 }
