6c798df51705af706a3fd6c301854819cd1a4eb1
thasmee <thasmee@dsv.su.se>
2014-08-09 13:06:54 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The  makeCacheMatrix function creates a matrix object that can cache its inverse 
+## The input matrix is assumed to be a square matrix
+#################################################### 
+ makeCacheMatrix <- function(x = matrix()) {
+-
++  s <- NULL
++  set <- function(y){
++    x <<- y
++    s <<- NULL
++  }
++  get <- function()x
++  setsolve <- function(solve)s <<- solve
++  getsolve <- function()s
++  list(set = set, get = get, setsolve = setsolve, getsolve =getsolve)
++  
+ }
+ 
+ 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
++## This function computes the inverse of the matrix returned if it has not been computed before
++##by the function makeCacheMatrix
+ ################################################
+ cacheSolve <- function(x, ...) {
+-        ## Return a matrix that is the inverse of 'x'
++  ## Return a matrix that is the inverse of 'x'
++  s <- x$getsolve()
++  if (!is.null(s)) {
++    message("getting cashed data")
++    return(s)
++  }
++  data <- x$get()
++  s <- solve(data, ...)
++  x$setsolve(s)
+ }
